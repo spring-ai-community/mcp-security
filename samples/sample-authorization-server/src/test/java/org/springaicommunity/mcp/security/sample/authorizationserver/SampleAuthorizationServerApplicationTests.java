98161fe5284c09c8c6953614a813a937beafb5c6
Daniel Garnier-Moiroux <git@garnier.wf>
2025-08-07 14:55:27 +0200

    Add sample authorization server
    Signed-off-by: Daniel Garnier-Moiroux <git@garnier.wf>

---
+++ samples/sample-authorization-server/src/test/java/org/springaicommunity/mcp/security/sample/authorizationserver/SampleAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springaicommunity.mcp.security.sample.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
