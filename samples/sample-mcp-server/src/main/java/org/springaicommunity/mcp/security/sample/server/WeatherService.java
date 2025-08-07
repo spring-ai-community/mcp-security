26503cbe78e92f553bba46546fde8b983b420985
Daniel Garnier-Moiroux <git@garnier.wf>
2025-08-07 15:27:19 +0200

    Add sample MCP server
    Signed-off-by: Daniel Garnier-Moiroux <git@garnier.wf>

---
+++ samples/sample-mcp-server/src/main/java/org/springaicommunity/mcp/security/sample/server/WeatherService.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2025 - 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springaicommunity.mcp.security.sample.server;
+
+import java.time.LocalDateTime;
+
+import org.springframework.ai.chat.model.ToolContext;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+/**
+ * @author Christian Tzolov
+ */
+@Service
+class WeatherService {
+
+	private final RestClient restClient;
+
+	public WeatherService() {
+		this.restClient = RestClient.create();
+	}
+
+	/**
+	 * The response format from the Open-Meteo API
+	 */
+	public record WeatherResponse(Current current) {
+		public record Current(LocalDateTime time, int interval, double temperature_2m) {
+		}
+	}
+
+	@Tool(description = "Get the temperature (in celsius) for a specific location")
+	public WeatherResponse getTemperature(@ToolParam(description = "The location latitude") double latitude,
+			@ToolParam(description = "The location longitude") double longitude, ToolContext toolContext) {
+
+		WeatherResponse weatherResponse = restClient.get()
+			.uri("https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m",
+					latitude, longitude)
+			.retrieve()
+			.body(WeatherResponse.class);
+
+		return weatherResponse;
+
+	}
+
+}
