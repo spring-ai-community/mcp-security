b79f4f07ea832a5fec5fc7952bd6a38ec219a145
Daniel Garnier-Moiroux <git@garnier.wf>
2025-08-19 18:04:19 +0200

    Samples: add empty "MCP client"

---
+++ samples/sample-mcp-client/src/main/java/org/springaicommunity/mcp/security/sample/client/SampleMcpClientApplication.java
@@ -0,0 +1,13 @@
+package org.springaicommunity.mcp.security.sample.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMcpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMcpClientApplication.class, args);
+	}
+
+}
