838c3521b4269eb8da4f5b5ccb2e14bcd772a903
Daniel Garnier-Moiroux <git@garnier.wf>
2025-08-19 17:42:44 +0200

    Samples: introduce MCP server where only the tools are secured
    Signed-off-by: Daniel Garnier-Moiroux <git@garnier.wf>

---
+++ samples/sample-mcp-server-secured-tools/src/main/java/org/springaicommunity/samplemcpserversecuredtools/McpServerConfiguration.java
@@ -0,0 +1,53 @@
+package org.springaicommunity.samplemcpserversecuredtools;
+
+import java.util.List;
+
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import static org.springaicommunity.mcp.security.resourceserver.config.McpResourceServerConfigurer.mcpServerAuthorization;
+
+@Configuration
+@EnableMethodSecurity
+public class McpServerConfiguration {
+
+	@Bean
+	public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+		return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http,
+			@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerUrl) throws Exception {
+		return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+			.with(mcpServerAuthorization(), (mcpAuthorization) -> {
+				mcpAuthorization.authorizationServer(issuerUrl).resourceIdentifier("http://localhost:8091/sse");
+			})
+			// MCP inspector
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+			.csrf(CsrfConfigurer::disable)
+			.build();
+	}
+
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOriginPatterns(List.of("*"));
+		configuration.setAllowedMethods(List.of("*"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+}
---
+++ samples/sample-mcp-server-secured-tools/src/test/java/org/springaicommunity/samplemcpserversecuredtools/SampleMcpServerSecuredToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springaicommunity.samplemcpserversecuredtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMcpServerSecuredToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
